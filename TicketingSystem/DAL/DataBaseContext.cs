510c4c63f16d19a8306481f53b5d6510d02b7f04
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-11-11 15:39:07 -0500

    the DAL folder is created and the Entity and Tickets classes are added, DataBaseContext is created to create the connection and the DB

---
+++ TicketingSystem/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.DAL.Entities;
+
+namespace TicketingSystem.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
---
+++ TicketingSystem/DAL/Entities/Tickets.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketingSystem.DAL.Entities
+{
+    public class Tickets : Entity
+    {
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
+        public string? EntranceGate { get; set; }
+
+    }
+}
